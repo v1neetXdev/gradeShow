6d5a133f912bfb992de08859958000c5b3135b63
vineet vishwakarma <vineetvishwakarma0168@gmail.com>
2026-07-08 22:07:58 +0530

    changes the ui improvement for desktop and mobile both

--- src/App.jsx
+++ src/App.jsx
@@ -25,10 +25,10 @@ const App = () => {
     }
     else if (marks >= 50 && marks <= 59) {
       setAns("D , FAIL")
-      setStatus("Average")
+      setStatus("Average , Need Imporovement")
     }
     else {
-      setAns("Z , FAIL")
+      setAns("E , FAIL")
       setStatus("........ Poor")
     }
     setTask('')
@@ -36,45 +36,39 @@ const App = () => {
   }
   return (
     <>
-    <div className="w-full bg-pink-700 py-5">
-    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-6xl text-center text-white font-bold py-5 px-3">
-  GradeShow made with ❤️
-</h1>
-</div>
-    <div className='flex flex-wrap flex-col justify-center items-center '>
-      <form onSubmit={buttonHandler} className='flex flex-wrap flex-col justify-center  bg-white text-black p-30 h-full'>
-        <div className='h-3- w-full bg-pink-700 absolute top-0 left-0 '>
-        <h1 className='text-6xl text-center text-white font-bold p-5'>GradeShow made with ❤️</h1>
-        </div>
+    <div className='w-full bg-pink-700 h-40'>
+      <h1 className='font-bold text-center py-3 px-1 text-5xl'>GradeShow made with ❤️</h1>
+    </div>
+    
+      <form onSubmit={buttonHandler} className='flex flex-wrap flex-col justify-center items-center bg-white text-black m-10 h-full'>
         <h1 className='text-center font-bold text-5xl my-5'>Grade calculator</h1>
-        <div className='h-30 flex flex-wrap flex-col justify-evenly w-full hover:scale-105 transition-all'>
         <input
-        className='py-2 px-1 border-3 border-blue-400 outline-blue-800 rounded-2xl font-light'
+        className='py-2 px-1 border-3 border-blue-400 outline-blue-800 rounded-2xl font-light m-3 w-[30%] transition-all'
           type="text"
           placeholder="Enter your name"
           value={naam}
           onChange={(e) => setNaam(e.target.value)}
         />
         <input
-        className='py-2 px-1 border-3 border-blue-400 outline-blue-800 rounded-2xl font-light'
+        className='py-2 px-1 border-3 border-blue-400 outline-blue-800 rounded-2xl font-light m-3 w-[30%] transition-all'
           type="number"
           placeholder="Enter your marks"
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
-        </div>
+        
         <div className='flex flex-wrap flex-row justify-center'>
         <button className='text-white bg-pink-600 p-2 font-bold hover:bg-pink-800 active:scale-95 transition-all rounded-3xl w-30 '>Submit</button>
         </div>
-        <div className='border-2 border-pink-800 shadow-2xl hover:scale-110 transition-all  mt-3 h-30 bg-pink-200 rounded-2xl flex flex-wrap flex-row justify-evenly'>
-            <div className='flex flex-wrap flex-col justify-evenly items-center min-p-5'>
+        <div className='border-2 border-pink-800 shadow-2xl hover:scale-101 transition-all mt-3 h-30 bg-pink-200 rounded-2xl flex flex-wrap flex-row justify-evenly w-[40%]'>
+            <div className='flex flex-wrap flex-col justify-evenly items-center '>
             <h1 className='font-bold'>Name : {subnaam}</h1>
             <h1 className='font-bold'>Marks : {ans}</h1>
             <h1 className='font-bold'>Status : {status}</h1>
           </div>
         </div>
       </form>
-    </div>
+    
     </>
   )
 }
